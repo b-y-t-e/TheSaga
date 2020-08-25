85d370ffc27c8b744ab39ec2534bed99e9030e71
else-andrzej <andrzej.bol@gmail.com>
2020-08-25 15:07:18 +0200

    dodanie historii

--- TheSaga/TheSaga/SagaStates/ISagaState.cs
+++ TheSaga/TheSaga/SagaStates/ISagaState.cs
@@ -10,19 +10,6 @@ namespace TheSaga.SagaStates
         public string CurrentStep { get; set; }
         public bool IsCompensating { get; set; }
         public Exception CurrentError { get; set; }
-        public IList<SagaStepLog> History { get; set; }
-    }
-
-    public class SagaStepLog
-    {
-        public bool HasSucceeded { get; set; }
-        public Exception Error { get; set; }
-        public string StateName { get; set; }
-        public string StepName { get; set; }
-        public DateTime Created { get; set; }
-        public bool HasFinished { get; set; }
-        public string NextStepName { get; set; }
-        public bool IsCompensating { get; set; }
-        public bool Async { get; set; }
+        public IList<SagaStepHistory> History { get; set; }
     }
 }
---
+++ TheSaga/TheSaga/SagaStates/SagaStepHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TheSaga.SagaStates
+{
+    public class SagaStepHistory
+    {
+        public bool HasSucceeded { get; set; }
+        public Exception Error { get; set; }
+        public string StateName { get; set; }
+        public string StepName { get; set; }
+        public DateTime Created { get; set; }
+        public bool HasFinished { get; set; }
+        public string NextStepName { get; set; }
+        public bool IsCompensating { get; set; }
+        public bool Async { get; set; }
+    }
+}
