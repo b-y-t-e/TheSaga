29a06119bb9fc097f54792eab41b6929e9981a54
whitebyte <andrzej.bol@gmail.com>
2021-07-06 14:04:21 +0200

    remove unused class

--- TheSaga/TheSaga/Exceptions/SagaIsBusyHandlingStepException.cs
+++ TheSaga/TheSaga/Exceptions/SagaIsBusyHandlingStepException.cs
@@ -3,18 +3,4 @@ using System.Runtime.Serialization;
 
 namespace TheSaga.Exceptions
 {
-    [Serializable]
-    public class SagaIsBusyHandlingStepException : Exception
-    {
-        public SagaIsBusyHandlingStepException(Guid id, string state, string step) :
-            base($"Saga {id} in state {state} is busy (handling step {step})")
-        {
-        }
-
-        protected SagaIsBusyHandlingStepException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
-        {
-        }
-    }
 }
